9c2503db462c3f8dcfa67edffd5b3bc3d5b30774
Hariprasad1998 <121543320+Hariprasad1998@users.noreply.github.com>
2022-12-28 17:36:40 +0530

    Add files via upload

---
+++ for loop .cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x, y;
+        for (x = 1; x <= 5; x++)
+        {
+            for (y = 1; y <= x; y++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadLine(); ;
+    }
+}
